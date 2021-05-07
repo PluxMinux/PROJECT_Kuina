14a96abcfbf6115876d8ad3c442a915a9358f2f7
PnM <64911289+PluxMinux@users.noreply.github.com>
2021-05-07 04:54:33 -0800

    List added
    List added to identify "Kuina"

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
-engine.say("yes? how can i help?")
-engine.runAndWait()
+
+# engine.say("Kuina")
+# engine.runAndWait()
 try:
     with sr.Microphone() as source:
-        print("Listening...")
+        print("Kuina is listening...")
         voice = listener.listen(source)
         command = listener.recognize_google(voice)
         command = command.lower()
-        if 'kuina' in command:
-            engine.say("yes? how can i help?")
+        print(command)
+        kuina = ['hey kuina','hey uwi na','hey queena','hey quina']
+        if command in kuina:
+            engine.say("yes?")
             engine.runAndWait()
             print(command)
-
 except:
     pass
